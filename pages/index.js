d6a127a2a7e2569c2523405c41646d80604212df
Marcos Cruz <32857539+marvim-macario@users.noreply.github.com>
2024-12-03 02:27:58 +0000

    Formatação arquivo index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 function Home() {
-   return <h1>"Sabe por que o elefante não usa computador? Porque ele tem medo de mouse"</h1>
+   return <h1>"Sabe por que o elefante não usa computador? 
+      Porque ele tem medo de mouse"</h1>
 }
 export default Home;
